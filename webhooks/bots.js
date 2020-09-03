c96a247d2786cc34fce5c3b3039696c679fc5919
imdkbj <46839821+imdkbj@users.noreply.github.com>
2020-09-03 23:25:36 +0530

    Added bot.launch for webhook

--- webhooks/bots.js
+++ webhooks/bots.js
@@ -1,4 +1,4 @@
-module.exports.handleUpdate = (bot) => {
+module.exports.handleUpdate = (bot,token) => {
     bot.hears('hi', ({
         reply
     }) => reply('Hello'));
@@ -6,5 +6,11 @@ module.exports.handleUpdate = (bot) => {
     bot.on('text', (ctx) => ctx.reply(ctx.message.text));
 
     bot.catch((e) => console.error(e))
-
+    
+    bot.launch({
+        webhook: {
+            hookPath: `${mydomain}allbots=bot${token}`,
+        },
+    });
+    
 }
