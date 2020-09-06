90bf383e0fe699361044c6f01bbe78744c20671e
imdkbj <46839821+imdkbj@users.noreply.github.com>
2020-09-06 15:10:09 +0530

    added custom session

--- webhooks/bots.js
+++ webhooks/bots.js
@@ -1,4 +1,12 @@
+const MySQLSession = require('telegraf-session-mysql')
+
+const session = new MySQLSession(Object.assign(MEMBER_DB, {
+    getSessionKey: (ctx) => `${ctx.chat.id}:${ctx.tg.token.split(":")[0]}`
+}));
+
 module.exports.handleUpdate = (bot,token) => {
+    bot.use(session.middleware());
+    
     bot.hears('hi', ({
         reply
     }) => reply('Hello'));
